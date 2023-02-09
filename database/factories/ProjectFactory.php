2d209e44d03142c6d021b7c08fcd279030b7d194
sca <posta.email3001@gmail.com>
2023-02-09 17:08:59 +0100

    creata migration per table project creati e impostati anche model, factory e seeder

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->word(),
+            'description' => fake()->paragraph(),
+            'repo_link' => fake()->url(),
+            'release_date' => fake()->dateTime(),
+            'main_image' => fake()->imageUrl(640, 480, 'animals', true)
+        ];
+    }
+}
