55808769a71d56974e40b4b280d866012f7eb637
sca <posta.email3001@gmail.com>
2023-02-09 12:12:15 +0100

    scaffolding progetto

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Usando Vite -->
+    @vite(['resources/js/app.js'])
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+    
+</head>
+<body>
+    <div id="app">
+
+        @include('components.header')
+
+        <main>
+            @yield('content')
+        </main>
+
+        @include('components.footer')
+        
+    </div>
+</body>
+</html>
